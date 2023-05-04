38f8945f4339ed9a382ffb4a944d6e98c3a08698
hepan05 <hepan05@kuaishou.com>
2023-05-04 11:30:44 +0800

    feat: 添加prisma模块

---
+++ prisma/migrations/20230504032539_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `age` TINYINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
