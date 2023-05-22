0a2982587ca1a445754db8d72632ae26dc750f34
hepan05 <hepan05@kuaishou.com>
2023-05-22 11:12:53 +0800

    feat: 查询用户的时候，不应该返回密码

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './user.dto';
 import { PrismaService } from '../prisma/prisma.service';
-
+const userSelectOption = { username: true, age: true, role: true };
 @Injectable()
 export default class UserService {
   constructor(private readonly prisma: PrismaService) {}
   createUser(user: CreateUserDto) {
     return this.prisma.user.create({
       data: user,
+      select: userSelectOption,
     });
   }
   findAll() {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      select: userSelectOption,
+    });
   }
 
   async findByUsername(username: string) {
-    return this.prisma.user.findUnique({ where: { username } });
+    return this.prisma.user.findUnique({
+      where: { username },
+      select: userSelectOption,
+    });
   }
 }
